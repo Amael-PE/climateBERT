bd88c0cda9e0e17c924ee009ff96750b071d4ac2
Amael-PE <amael@amael.io>
2023-05-28 14:35:57 +0200

    Adapt inference script to needs of the Notebook test

--- src/inference_script.py
+++ src/inference_script.py
@@ -5,6 +5,7 @@ import json
 import torch
 from tqdm import tqdm
 import pandas as pd
+from typing import List
 
 class SequenceClassificationDataset(Dataset):
 	def __init__(self, x, tokenizer):
@@ -21,35 +22,17 @@ class SequenceClassificationDataset(Dataset):
 		return {"model_inputs": model_inputs}
 
 
-if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--model_name', type=str, default='climatebert-environmental-claims')
-	parser.add_argument('--outfile_name', type=str, default='environmental_claim_predictions.csv')
-	parser.add_argument('--filename', type=str, default="data/test.jsonl")
-	args = parser.parse_args()
-
+def infer(sentences: List[str], model_name='climatebert-environmental-claims'):
 	device = "cuda" if torch.cuda.is_available() else "cpu"
 
 	# load model and tokenizer
 	try:
-		tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+		tokenizer = AutoTokenizer.from_pretrained(model_name)
 	except:
 		tokenizer = AutoTokenizer.from_pretrained("roberta-base")
 
-	model = AutoModelForSequenceClassification.from_pretrained(args.model_name).to(device)
+	model = AutoModelForSequenceClassification.from_pretrained(model_name).to(device)
 
-	# load sentences to predict
-	
-
-	if args.filename.endswith("jsonl"):
-		df = pd.read_json(args.filename, lines = True)
-		if "text" in df.columns:
-			sentences = df.text.tolist()
-		elif "sentences" in df.columns:
-			sentences = df.text.tolist()
-	elif args.filename.endswith(".txt"):
-		with open(args.filename) as f:
-			sentences = [i for i in f]
 				
 	predict_dataset = SequenceClassificationDataset(sentences, tokenizer)
 
@@ -63,8 +46,31 @@ if __name__ == "__main__":
 			outputs.extend(logits.argmax(dim=1).tolist())
 			probs.extend(logits.softmax(dim=1)[:,1].tolist())
 	
-	# save to outfile
 
-	df = pd.DataFrame(list(zip(sentences, outputs, probs)), columns=["sentence", "classification", "probability"])
+	return pd.DataFrame(list(zip(sentences, outputs, probs)), columns=["sentence", "classification", "probability"])
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--model_name', type=str, default='climatebert-environmental-claims')
+	parser.add_argument('--outfile_name', type=str, default='environmental_claim_predictions.csv')
+	parser.add_argument('--filename', type=str, default="data/test.jsonl")
+	args = parser.parse_args()
+
+	# load sentences to predict
+	if args.filename.endswith("jsonl"):
+		df = pd.read_json(args.filename, lines = True)
+		if "text" in df.columns:
+			sentences = df.text.tolist()
+		elif "sentences" in df.columns:
+			sentences = df.text.tolist()
+	elif args.filename.endswith(".txt"):
+		with open(args.filename) as f:
+			sentences = [i for i in f]
+
+	# Infer 
+	df = infer(sentences, args.model_name)
+	
+	# save to outfile
 	df.to_csv(args.outfile_name, index=False)
 
